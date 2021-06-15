c3f44c7c81e646f489897c7607a757efe9fcfd1b
Justin Seitz <justinseitz2016@gmail.com>
2021-06-15 15:38:08 -0700

    counter that increments

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Interview practice questions
+# Interview practice questions
 1) Create a button that increments a counter.
 2) Load and display raw JSON data from API
--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        };
+        this.increment = this.increment.bind(this);
+    }
+
+    increment() {
+        this.setState({ counter: this.state.counter+1 });
+    }
+
     render(){
         return(
+            <>
             <h1>Hello world</h1>
+            <div>
+                <button onClick={this.increment}>Increment</button>
+                <p>{this.state.counter}</p>
+            </div>
+            </>
         );
     }
 }
