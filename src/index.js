d477427f99ee7d4ed03923d50a143be1985934a1
Justin Seitz <justinseitz2016@gmail.com>
2021-06-15 20:24:40 -0700

    now displays json data

--- src/index.js
+++ src/index.js
@@ -5,13 +5,30 @@ class MyComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            counter: 0
+            counter: 0,
+            data: ''
         };
         this.increment = this.increment.bind(this);
+        this.getData = this.getData.bind(this);
     }
 
     increment() {
         this.setState({ counter: this.state.counter+1 });
+    }
+    getData(e) {
+        const url = 'https://api.datamuse.com/words?'
+        const queryParams = 'rel_jjb=';
+        const wordQuery = e.target.value;
+        const endPoint = url + queryParams + wordQuery;
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                this.setState({data: JSON.stringify(xhr.response)});
+            }
+        };
+        xhr.open('GET', endPoint);
+        xhr.send();
     }
 
     render(){
@@ -21,6 +38,12 @@ class MyComponent extends React.Component {
             <div>
                 <button onClick={this.increment}>Increment</button>
                 <p>{this.state.counter}</p>
+            </div>
+            <div>
+                <input 
+                    type='text'
+                    onChange={this.getData}></input>
+                <p>{this.state.data}</p>
             </div>
             </>
         );
